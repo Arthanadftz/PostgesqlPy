c88fc9e4fafbbebbe9dafbcb969f34ea33b3c2bd
Arthanadftz <43166097+Arthanadftz@users.noreply.github.com>
2018-09-29 13:50:33 +0300

    Delete t2.py

--- t2.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import connpy
-
-class ConnpyTest(unittest.TestCase):
-	def test_mean(self):
-		self.assertEqual(connpy.mean_val(), 0.4714999999999999)
-
-if __name__ == '__main__':
-	unittest.main()
